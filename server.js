5a1d9c89b4b4628cf0ed36cbcb7fc0045100c345
JBShort <81993517+JBShort@users.noreply.github.com>
2021-11-11 15:12:48 -0800

    updated server.js

--- server.js
+++ server.js
@@ -6,5 +6,7 @@ const port = 8000;
 app.use(cors(), express.json(), express.urlencoded({ extended: true }));
 
 // include middleware here
+require('./server/config/mongoose.config');
+require('./server/routes/items.routes')(app);
 
 app.listen(port, () => console.log(`The server is all fired up on port UwU ${port}`));
