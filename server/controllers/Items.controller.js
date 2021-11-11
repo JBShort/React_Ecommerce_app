4b70272707b3b7d66bd3bfb29bb1dea1167b6354
JBShort <81993517+JBShort@users.noreply.github.com>
2021-11-10 16:53:22 -0800

    controller finished

--- server/controllers/Items.controller.js
+++ server/controllers/Items.controller.js
@@ -26,4 +26,38 @@ module.exports = {
                 res.status(400).json(err);
             })
     },
+
+    // create new item
+    create: (req, res) => {
+        Items.create(req.body)
+            .then(newItem => {
+                console.log("Created new Item: ", newItem);
+                res.json(newItem)
+            })
+            .catch(err => {
+                console.log("error: ", err)
+                res.status(400).json(err);
+            })
+    },
+
+    // update item
+    update: (req, res) => {
+        Items.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true })
+            .then(updatedItem => {
+                console.log("updated Item: ", updatedItem)
+                res.json(updatedItem)
+            })
+            .catch(err => {
+                console.log("Error on updating: ", err)
+                res.json(400).json(err);
+            })
+    },
+
+    // delete a item
+    deleteSingle: (req,res) => {
+        console.log("yeeting: ", req.params.id);
+        Items.deleteOne({ _id: req.params.id })
+            .then(result => res.json({result: result}))
+            .catch(err => res.status(400).json({message: "deletion error", error: err}))
+    }
 }
